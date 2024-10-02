b745999278f7d7b5a621e160ef68a6c8e8bc0736
vynious <shawnthiah@gmail.com>
2024-10-02 19:57:17 +0800

    scaffold

---
+++ src/main/java/com/example/ticketdestroyer/TicketDestroyerApplication.java
@@ -0,0 +1,13 @@
+package com.example.ticketdestroyer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketDestroyerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketDestroyerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ticketdestroyer/TicketDestroyerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ticketdestroyer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketDestroyerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
